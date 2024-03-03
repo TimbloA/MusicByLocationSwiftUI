e774ff4934f1ba1d8a81a987c8df711061343e21
Adi Timblo <timblo.a@etoncollege.org.uk>
2024-03-03 10:31:58 +0000

    Initial Commit

---
+++ MusicByLocationSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicByLocationSwiftUI
+//
+//  Created by Timblo, Adi (WING) on 03/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicByLocationSwiftUI/MusicByLocationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationSwiftUIApp.swift
+//  MusicByLocationSwiftUI
+//
+//  Created by Timblo, Adi (WING) on 03/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
